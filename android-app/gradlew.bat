68e99354e6d298f4e4112f9b4779f50b1354c205
Aajadh Panigrahi <azadpanigrahi469@gmail.com>
2026-08-15 22:27:14 +0530

    Add files via upload

--- android-app/gradlew.bat
+++ android-app/gradlew.bat
@@ -1,6 +1,15 @@
+<<<<<<< HEAD
 @echo off
 setlocal enabledelayedexpansion
 set DIR=%~dp0
 set JAVA_EXE=java
 if defined JAVA_HOME set JAVA_EXE=%JAVA_HOME%\bin\java
 %JAVA_EXE% -classpath "%DIR%\gradle\wrapper\gradle-wrapper.jar" org.gradle.wrapper.GradleWrapperMain %*
+=======
+@echo off
+setlocal enabledelayedexpansion
+set DIR=%~dp0
+set JAVA_EXE=java
+if defined JAVA_HOME set JAVA_EXE=%JAVA_HOME%\bin\java
+%JAVA_EXE% -classpath "%DIR%\gradle\wrapper\gradle-wrapper.jar" org.gradle.wrapper.GradleWrapperMain %*
+>>>>>>> 6e80ed9 (Add .gitattributes to enforce consistent line‑ending handling)
